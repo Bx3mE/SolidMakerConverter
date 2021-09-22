3c88172a9e25179b22e9a8b1b7e818b23b46a5a1
RogueNeurons <c.daniel.olsson@gmail.com>
2021-09-23 00:13:00 +0200

    Update Program.cs
    Copyright...

--- SolidMakerConverter/Program.cs
+++ SolidMakerConverter/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+/*
+  SolidMakerConverter - A tool to be used for parsing sliced files from
+  Cura 4.9.1 for the discontinued Solidmaker SLA 3D Printer
+  
+  Copyright (c) 2020-2021 Daniel Olsson
+  
+  SolidMakerConverter is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  SolidMakerConverter is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with SolidMakerConverter. If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
